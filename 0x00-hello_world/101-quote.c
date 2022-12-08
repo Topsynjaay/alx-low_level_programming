9f742b115cdfd7e7d2b45ea455e72f8e8304f80c
Temitope Daramola <topsynjaay@gmail.com>
2022-12-08 11:06:36 -0800

    c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
-
-    #include <unistd.h>                                                                                                                      
+#include <unistd.h>                                                                                                                      
 
 /** 
  *
@@ -14,11 +13,8 @@ int main(void)
 
 {
 
-	 write(1, "and that piece of art is useful""\" - Dora Korpar, 2015-10-19\n", 59);
-
-	  write(1, "and that piece of art is useful""\" - Dora Korpar, 2015-10-19\n", 59);
-
-	   return (1);
+	 write(0, "and that piece of art is useful""\" - Dora Korpar, 2015-10-19\n", 59);
+	   return (0);
 
 }
 
